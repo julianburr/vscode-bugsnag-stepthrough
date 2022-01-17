b180aaefa2d7ebf27ef97dcfa6841077ca91b5bc
Julian Burr <julian.burr@rexsoftware.com.au>
2022-01-17 15:07:54 +1000

    Move SEO back inline to fix weird nextjs issue (#9)

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -3,11 +3,19 @@ import Head from "next/head";
 import Script from "next/script";
 
 import { usePageView } from "src/utils/ga";
-import { SEO } from "src/components/seo";
+
+import favicon from "src/assets/favicon.png";
+import previewPng from "src/assets/images/preview.png";
 
 function MyApp({ Component, pageProps }: AppProps) {
   // Track page view events in Google Analytics
   usePageView();
+
+  const title = "VS Code Bugsnag Stepthrough";
+  const desc =
+    "The VS Code extension that helps you get on top of your open issues in Bugsnag";
+
+  const preview = `${process.env.API_BASE_URL}${previewPng.src}`;
 
   return (
     <>
@@ -20,7 +28,24 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
         <meta name="robots" content="all" />
 
-        <SEO />
+        {/* Basics */}
+        <title>{title}</title>
+        <meta name="description" content={desc} />
+        <meta name="keywords" content="vscode, bugsnag, extension, react" />
+        <link rel="icon" href={favicon.src} type="image/png" />
+
+        {/* Open Graph */}
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={desc} />
+        <meta property="og:image" content={preview} />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter */}
+        <meta property="twitter:image" content={preview} />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:creator" content="@jburr90" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={desc} />
 
         {/* Add Google Font incl DNS prefetch for perf improvements */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
